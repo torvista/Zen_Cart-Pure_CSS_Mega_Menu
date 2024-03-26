9be726566e92a68f7d7056cb163d70de3f59ac06
Steve <4391026+torvista@users.noreply.github.com>
2024-03-26 18:24:22 +0100

    fix: mis-declared constants

--- files/includes/languages/english/extra_definitions/responsive_classic/headermenu.php
+++ files/includes/languages/english/extra_definitions/responsive_classic/headermenu.php
@@ -16,40 +16,43 @@
 // | to obtain it through the world-wide-web, please send a note to       |
 // | license@zen-cart.com so we can mail you a copy immediately.          |
 // +----------------------------------------------------------------------+
-// $Id: headermenu.php  2004-07-11 DrByteZen $
-// modified by www.picaflor-azul.com
+// $Id: headermenu.php  2024-03-26 torvista
+
 
 //text descriptions for horizontal header menu -- can be relocated if desired
 //These are used in conjunction with the various <li> tags in templates/YOURTEMPLATE/common/tpl_mega_menu.php
 
 /*bof quick links definitions*/
-define('HEADER_TITLE_NEW_PRODUCTS','New Products');
-define('HEADER_TITLE_ALL_PRODUCTS','All Products');
-define('HEADER_TITLE_SPECIALS','Specials');
-define('HEADER_TITLE_SEARCH','Search');
-define('HEADER_TITLE_QUICK_LINKS','Quick Links');
+define('HEADER_TITLE_NEW_PRODUCTS', 'New Products');
+define('HEADER_TITLE_ALL_PRODUCTS', 'All Products');
+define('HEADER_TITLE_SPECIALS', 'Specials');
+define('HEADER_TITLE_SEARCH', 'Search');
+define('HEADER_TITLE_QUICK_LINKS', 'Quick Links');
 
 /*bof categories definitions*/
-define('HEADER_TITLE_CATEGORIES','Shop');
+define('HEADER_TITLE_CATEGORIES', 'Shop');
 
 /*bof manufacturers definitions*/
-define('HEADER_TITLE_MANUFACTURERS','Brands');
+define('HEADER_TITLE_MANUFACTURERS', 'Brands');
 
 /*bof about us definitions*/
-define('HEADER_TITLE_ABOUT_US','About Us');
-define('TITLE_ABOUT_US','Welcome to our Store!');
-define('TEXT_ABOUT_US','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam interdum feugiat ipsum vehicula sollicitudin. Integer sed lacus eget risus consectetur ullamcorper. Pellentesque rutrum ullamcorper faucibus. Nam porttitor iaculis enim, mattis tristique velit tristique bibendum.  Aliquam porta nisl tortor, non luctus justo. Nam tincidunt dui vel mauris tincidunt posuere. Phasellus rhoncus elit et lorem sodales ullamcorper.');
-define(ABOUT_US_IMAGE,'aboutus.png');
+define('HEADER_TITLE_ABOUT_US', 'About Us');
+define('TITLE_ABOUT_US', 'Welcome to our Store!');
+define(
+    'TEXT_ABOUT_US',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam interdum feugiat ipsum vehicula sollicitudin. Integer sed lacus eget risus consectetur ullamcorper. Pellentesque rutrum ullamcorper faucibus. Nam porttitor iaculis enim, mattis tristique velit tristique bibendum.  Aliquam porta nisl tortor, non luctus justo. Nam tincidunt dui vel mauris tincidunt posuere. Phasellus rhoncus elit et lorem sodales ullamcorper.'
+);
+define('ABOUT_US_IMAGE', 'aboutus.png');
 
 /*bof information definitions*/
-define('HEADER_TITLE_INFORMATION','Info');
-define('TITLE_EZ_PAGES','Useful Links');
-define('TITLE_GENERAL','General Info');
-define('TITLE_CUSTOMERS','Customers');
-define('TITLE_NEWSLETTERS','Newsletters');
+define('HEADER_TITLE_INFORMATION', 'Info');
+define('TITLE_EZ_PAGES', 'Useful Links');
+define('TITLE_GENERAL', 'General Info');
+define('TITLE_CUSTOMERS', 'Customers');
+define('TITLE_NEWSLETTERS', 'Newsletters');
 
 /*bof connect with us*/
-define('TITLE_CONNECT','Connect');
+define('TITLE_CONNECT', 'Connect');
 
 /*bof connect with us icons*/
 define('FACEBOOK_ICON_SM', 'facebook_sm.png');
@@ -68,29 +71,25 @@ define('GOOGLE_TEXT', 'Recommend Us');
 define('BLOG_TEXT', 'Our Blog');
 
 /*bof connect with us urls*/
-define('FACEBOOK','http://www.facebook.com/Custom.Zen.Cart.Design');
+define('FACEBOOK', 'http://www.facebook.com/Custom.Zen.Cart.Design');
 define('TWITTER', 'http://www.twitter.com/picaflorazul');
 define('YOUTUBE', 'http://www.youtube.com/user/ZenCartEasyHelp');
 define('PINTEREST', 'http://www.pinterest.com/picaflorazul');
 define('GOOGLE', 'https://plus.google.com/113609090217058276980/posts');
 define('BLOG', 'http://www.picaflor-azul.com/blog');
 
-
 /*bof contact us form definitions*/
-define('HEADER_TITLE_CONTACT_US','Contact Us');
-define('TITLE_CONTACT_US','Have a Question?');
-define('TEXT_CONTACT_US','Your short message goes here.');
+define('HEADER_TITLE_CONTACT_US', 'Contact Us');
+define('TITLE_CONTACT_US', 'Have a Question?');
+define('TEXT_CONTACT_US', 'Your short message goes here.');
 
 /*bof customer service*/
-define('HEADER_TITLE_CUSTOMER_SERVICE','Customer Service');
-define('TITLE_SHIPPING','Shipping');
-define('TEXT_SHIPPING_INFO','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam interdum feugiat ipsum vehicula sollicitudin. Integer sed lacus eget risus consectetur ullamcorper. Pellentesque rutrum ullamcorper faucibus. Nam porttitor iaculis enim, mattis tristique velit tristique bibendum. ');
-define('TITLE_CONFIDENCE','Shop with Confidence');
-define('TEXT_CONFIDENCE','Here you can write a few short sentencs about why your customers can buy with conficence from your store.  You might also want to outline your payment options here.');
-define(PAYMENT_ICON,'payments.png');
-
-
-
-
-
-?>
+define('HEADER_TITLE_CUSTOMER_SERVICE', 'Customer Service');
+define('TITLE_SHIPPING', 'Shipping');
+define(
+    'TEXT_SHIPPING_INFO',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam interdum feugiat ipsum vehicula sollicitudin. Integer sed lacus eget risus consectetur ullamcorper. Pellentesque rutrum ullamcorper faucibus. Nam porttitor iaculis enim, mattis tristique velit tristique bibendum.'
+);
+define('TITLE_CONFIDENCE', 'Shop with Confidence');
+define('TEXT_CONFIDENCE', 'Here you can write a few short sentencs about why your customers can buy with conficence from your store. You might also want to outline your payment options here.');
+define('PAYMENT_ICON', 'payments.png');
